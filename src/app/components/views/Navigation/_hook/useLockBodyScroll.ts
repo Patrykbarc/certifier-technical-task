55cb7792f021f6d546609df13f9dd6fdcedef163
Patryk Barć <patrykbarc.design@gmail.com>
2025-07-27 00:19:11 +0200

    fix: prevent SSR window access in useLockBodyScroll

--- src/app/components/views/Navigation/_hook/useLockBodyScroll.ts
+++ src/app/components/views/Navigation/_hook/useLockBodyScroll.ts
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 
 export function useLockBodyScroll(isLocked: boolean) {
   useEffect(() => {
+    if (typeof window === 'undefined') return
+
     if (isLocked) {
       const originalOverflow = document.body.style.overflow
       document.body.style.overflow = 'hidden'
