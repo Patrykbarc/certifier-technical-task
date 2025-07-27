e6387acabd5c6a81a11211de8371a2dbff1b4a74
Patryk Barć <patrykbarc.design@gmail.com>
2025-07-27 13:26:22 +0200

    style: refine gradient background and adjust heading margin in Hero component

--- src/app/components/views/Hero/Hero.tsx
+++ src/app/components/views/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { HeroLogos } from './atoms/HeroLogos'
 
 export function Hero() {
   return (
-    <section className="bg-gradient-to-b from-[#E7F4EB] to-[#E7F4EB00] flex flex-col py-22 space-y-16">
+    <section className="bg-gradient-to-b from-[64%] from-[#E7F4EB] to-[#E7F4EB00] flex flex-col py-22 space-y-16">
       <div className="flex-1 flex justify-center pt-14 wrapper w-full">
         <HeroContent />
       </div>
