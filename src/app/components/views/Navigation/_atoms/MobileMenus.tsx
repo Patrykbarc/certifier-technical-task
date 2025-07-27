080a4f3948d395570e7a4b634486289adbb984cc
Patryk Barć <patrykbarc.design@gmail.com>
2025-07-27 18:29:07 +0200

    style: adjust padding for Link elements in MobileMenus component

--- src/app/components/views/Navigation/_atoms/MobileMenus.tsx
+++ src/app/components/views/Navigation/_atoms/MobileMenus.tsx
@@ -72,12 +72,12 @@ function MenuOpen({ isMobileMenuOpen, setIsMobileMenuOpen }: MenuProps) {
           {NAV_ITEMS.map((item) => (
             <li
               key={item.label}
-              className="w-full cursor-pointer py-6 text-lg font-medium text-black hover:text-blue-500 transition-colors border-b border-gray-200"
+              className="w-full cursor-pointer text-lg font-medium text-black hover:text-blue-500 transition-colors border-b border-gray-200"
             >
               <Link
                 key={item.label}
                 href={item.href}
-                className="pb-6"
+                className="py-6 w-full block"
                 onClick={() => setIsMobileMenuOpen(false)}
               >
                 {item.label}
